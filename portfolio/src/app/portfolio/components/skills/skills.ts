8c1942c6040d3ba2c431161170700ce3b357b0ab
Muhammad Afzal <afzalofficial911@gmail.com>
2025-06-14 19:35:37 +0500

    Add skills section and some libraries

--- portfolio/src/app/portfolio/components/skills/skills.ts
+++ portfolio/src/app/portfolio/components/skills/skills.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.html',
   styleUrl: './skills.css'
 })
-export class Skills {
+export class Skills implements OnInit{
+  skills = [
+    { name: 'Angular', percent: 50 },
+    { name: 'HTML', percent: 95 },
+    { name: 'CSS', percent: 85 },
+    { name: 'TypeScript', percent: 70 },
+    { name: 'SQL', percent: 85 },
+    { name: '.NET Core', percent: 70 }
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
 
 }
