2e40a697a75e5926363d02ee5654bcbaedacde3c
Muhammad Afzal <afzalofficial911@gmail.com>
2025-06-18 00:01:17 +0500

    Add contact section

--- portfolio/src/app/portfolio/components/contact/contact.ts
+++ portfolio/src/app/portfolio/components/contact/contact.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidationErrorComponent } from '../shared/validation-error/validation-error'; // Adjust the path as needed
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, ValidationErrorComponent],
   templateUrl: './contact.html',
-  styleUrl: './contact.css'
+  styleUrls: ['./contact.css']
 })
 export class Contact {
+  contactForm: FormGroup;
+  hovered = false;
+  submitted = false;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', [Validators.required, Validators.minLength(4)]],
+      message: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.contactForm.valid) {
+      console.log('Form Submitted:', this.contactForm.value);
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
 }
