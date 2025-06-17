c63f3bd111cbf3e50efccb9268fbf220613cd76b
Muhammad Afzal <afzalofficial911@gmail.com>
2025-06-18 00:01:57 +0500

    Add common validation

--- portfolio/src/app/app.ts
+++ portfolio/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ portfolio/src/app/portfolio/components/shared/validation-error/validation-error.ts
@@ -0,0 +1,34 @@
+import { Component, Input } from '@angular/core';
+import { AbstractControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-validation-error',
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  template: `
+    <div class="error" *ngIf="shouldShowError()">
+  <small *ngIf="control?.touched && control?.errors?.['required']">This field is required.</small>
+  <small *ngIf="control?.touched && control?.errors?.['minlength'] as err">Minimum {{ err['minlength'].requiredLength }} characters required.</small>
+  <small *ngIf="control?.touched && control?.errors?.['maxlength'] as err">Maximum {{ err['maxlength'].requiredLength }} characters allowed.</small>
+  <small *ngIf="control?.touched && control?.errors?.['email']">Invalid email format.</small>
+</div>
+
+  `,
+  styles: [`
+    .error {
+  min-height: 16px; /* Reserve enough height even when no error */
+  color: #f87171;
+  font-size: 12px;
+  margin-bottom: 10px;
+  display: block;
+}`]
+})
+export class ValidationErrorComponent {
+  @Input() control!: AbstractControl;
+  @Input() submitted = false;
+
+  shouldShowError(): boolean {
+     return !!(this.submitted && this.control && this.control.invalid);
+  }
+}
