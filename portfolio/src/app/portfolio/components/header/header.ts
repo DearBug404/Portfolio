915493ebc04e0c6061dc766ec7173eb537c77a54
Muhammad Afzal <afzalofficial911@gmail.com>
2025-06-19 16:08:08 +0500

    Add logo and scrolling functionality

--- portfolio/src/app/portfolio/components/header/header.ts
+++ portfolio/src/app/portfolio/components/header/header.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { elementAt } from 'rxjs';
 @Component({
   selector: 'app-header',
   imports: [CommonModule],
@@ -9,5 +9,29 @@ import { CommonModule } from '@angular/common';
 })
 export class Header {
   menuOpen: boolean = false;
+  clickedTab: string = 'home';
+  hoveredTab: string = '';
+  setHoveredTab(tab: string) {
+    this.hoveredTab = tab;
+  }
+  clearHoveredTab() {
+    this.hoveredTab = '';
+  }
+  setClickedTab(tab: string) {
+    this.clickedTab = tab;
+    this.menuOpen = false;
+  }
+  scrollTo(elementId: string): void {
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+  goHome(): void {
+  this.scrollTo('home');      // scroll to home
+  setTimeout(() => {
+    window.location.reload(); // refresh the page after scroll
+  }, 500); // delay ensures scroll happens first
+}
 
 }
